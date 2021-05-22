b5b54e6df3dcb5937c57722b328e23212e3b11ff
Katie Berkowitz <katieb@Katies-MacBook-Pro.local>
2021-05-22 14:46:25 -0400

    Edits

--- table_schema.sql
+++ table_schema.sql
@@ -1,4 +1,7 @@
---DROP TABLE departments
+--Departments Table
+
+DROP TABLE departments
+
 CREATE TABLE departments (
   dept_no VARCHAR(10),
   dept_name VARCHAR(30) NOT NULL,
@@ -8,16 +11,17 @@ CREATE TABLE departments (
 select * from departments
 
 
--- Create table employees
+-- Employees Table
 
---DROP TABLE employees
+DROP TABLE employees
 
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
+	emp_title_id VARCHAR(20) NOT NULL,
 	birth_date DATE NOT NULL,
 	first_name VARCHAR(20) NOT NULL,
 	last_name VARCHAR(20) NOT NULL,
-	gender VARCHAR(2) NOT NULL,
+	sex VARCHAR(2) NOT NULL,
 	hire_date DATE NOT NULL,
 	PRIMARY KEY (emp_no)
 );
@@ -25,31 +29,27 @@ CREATE TABLE employees (
 select * from employees
 
 
---Create table dept_emp
+--Department Employees table
 
---DROP TABLE dept_emp
+DROP TABLE dept_emp
 
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(10) NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 select * from dept_emp
 
 
--- Create table dept_managers
+-- Dept Managers Table
 
---DROP TABLE dept_managers
+DROP TABLE dept_managers
 
 CREATE TABLE dept_managers (
  dept_no VARCHAR (10),
  emp_no INT,
- from_date DATE NOT NULL,
- to_date DATE NOT NULL,
  FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
@@ -58,31 +58,26 @@ select * from dept_managers
 
 
 
---Creating salaries table
+--Salaries Table
 
---DROP TABLE salaries
+DROP TABLE salaries
 
 CREATE TABLE salaries (
-	emp_no BIGINT NOT NULL,
-	salary BIGINT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
 	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
 );
 
 select * from salaries
 
 
---Creating titles table
+--Titles Table
 
---DROP TABLE titles
+DROP TABLE titles
 
 CREATE TABLE titles (
-	emp_no INT NOT NULL,
-	title VARCHAR(20) NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+	title_id VARCHAR(20) PRIMARY KEY NOT NULL,
+	title VARCHAR(20) NOT NULL
 );
 
 select * from titles
